09731fc5e82dbd01a4be60d46c888f2460473310
chanmyaeoo <chanmyaeoo1996mgk@gmail.com>
2020-01-29 21:38:26 +0630

    added factorial challenge

---
+++ factorial/factorial.js
@@ -0,0 +1,9 @@
+const factorial = num => {
+  let result = 1;
+  for (let i = 1; i <= num; i++) {
+    result = result * i;
+  }
+  return result;
+};
+const number = 6;
+console.log(`Factorial of ${number} is ${factorial(number)}`);
